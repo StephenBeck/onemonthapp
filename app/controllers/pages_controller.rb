e5945e702d936b357849e6d32b4d04d152dc3388
Stephen Beck <stephen@enginedigital.com>
2015-01-11 22:05:21 -0500

    Customized Colors/Layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
 
   def about
   end
+  
+  def privacy
+  end
+  
+  def terms
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   root "pages#home"
   get "about" => "pages#about" # creates about path
+  get "privacy" => "pages#privacy" 
+  get "terms" => "pages#terms"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
