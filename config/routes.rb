8bda73abb580cac9e164e3d55caf674f5b597a52
Stephen Beck <stephen@enginedigital.com>
2015-02-01 03:28:14 -0500

    Update Show View / Add Glyph Icons / Change Root Path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about" # creates about path
   get "privacy" => "pages#privacy" 
   get "terms" => "pages#terms"
