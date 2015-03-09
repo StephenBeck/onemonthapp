1101105e602d4b12069ce54f9f9701b53d9f4bf4
Stephen Beck <stephen@enginedigital.com>
2015-03-09 00:14:35 -0400

    Made images responsive and added Gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def avatar_url(user)
+    #default_url = "#{root_url}/app/assets/images/missing-avatar.gif"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    #"http://gravatar.com/avatar/#{gravatar_id}.png?s=32&d=#{CGI.escape(default_url)}"
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=32"
+  end
+
 end
